fa24ac73cc3530b5a39ebe67eb8cb86ec3c575fc
Sarabveer Singh <me@sarabveer.me>
2022-11-18 14:45:00 -0500

    feat: implement nanakshahi calendar

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-A gnome shell extension for Linux that displays nepali date on the panel.
+A gnome shell extension for Linux that displays the Nanakshahi date on the panel.
 
 ## How it looks
 <img src="https://raw.githubusercontent.com/Biplab-Dutta/Nepali_Date/master/assets/center_top_panel.png"></img>
@@ -10,3 +10,15 @@ A gnome shell extension for Linux that displays nepali date on the panel.
 If you want to customize the look and feel to your preference, then navigate to `~/.local/share/gnome-shell/extensions/nepali-date@biplab/` and add a `css` file. You can also edit `extension.js` file if you know what you are doing because it might break the extension if improperly configured. 
 
 ### [🔗 Download Link](https://extensions.gnome.org/extension/4518/nepali-date/)
+
+## License
+
+Copyright (C) 2021-2022  Biplab Dutta
+
+Copyright (C) 2022  Sarabveer Singh
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation as per version 3 of the License.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
--- extension.js
+++ extension.js
@@ -4,108 +4,94 @@ const Mainloop = imports.mainloop;
 
 let text, button, sourceId = null;
 
-function _getNepaliDate() {
+// Check if current year is Leap Year (Gregorian Leap Rule)
+// Uses optimized version from: https://stackoverflow.com/a/11595914
 
-	nepcal = {
-		'74': { 'mon_days': [31, 31, 31, 32, 31, 31, 30, 29, 30, 29, 30, 30], 'tot_days': 365 },
-		'75': { 'mon_days': [31, 31, 32, 31, 31, 31, 30, 29, 30, 29, 30, 30], 'tot_days': 365 },
-		'76': { 'mon_days': [31, 32, 31, 32, 31, 30, 30, 30, 29, 29, 30, 30], 'tot_days': 365 },
-		'77': { 'mon_days': [31, 32, 31, 32, 31, 30, 30, 30, 29, 30, 29, 31], 'tot_days': 366 },
-		'78': { 'mon_days': [31, 31, 31, 32, 31, 31, 30, 29, 30, 29, 30, 30], 'tot_days': 365 },
-		'79': { 'mon_days': [31, 31, 32, 31, 31, 31, 30, 29, 30, 29, 30, 30], 'tot_days': 365 },
-		'80': { 'mon_days': [31, 32, 31, 32, 31, 30, 30, 30, 29, 29, 30, 30], 'tot_days': 365 },
-		'81': { 'mon_days': [31, 31, 32, 32, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 366 },
-		'82': { 'mon_days': [30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'83': { 'mon_days': [31, 31, 32, 31, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'84': { 'mon_days': [31, 31, 32, 31, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'85': { 'mon_days': [31, 32, 31, 32, 30, 31, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'86': { 'mon_days': [30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'87': { 'mon_days': [31, 31, 32, 31, 31, 31, 30, 30, 29, 30, 30, 30], 'tot_days': 366 },
-		'88': { 'mon_days': [30, 31, 32, 32, 30, 31, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'89': { 'mon_days': [30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-		'90': { 'mon_days': [30, 32, 31, 32, 31, 30, 30, 30, 29, 30, 30, 30], 'tot_days': 365 },
-	};
+function leapYear( year ) {
+  return ( year & 3 ) === 0 && ( ( year % 25 ) !== 0 || ( year & 15 ) === 0 );
+}
 
-	months = ['Baisakh', 'Jestha', 'Ashar', 'Shrawan', 'Bhadra', 'Ashoj', 'Kartik', 'Mangsir', 'Poush', 'Magh', 'Falgun', 'Chaitra'];
+function _getNanakshahiDate() {
 
-	ref_date_nep = [2074, 1, 1];
-	ref_date_eng = new Date(2017, 3, 14);
-	inp_date_eng = new Date();
+  // Taken from:
+  // https://github.com/sarabveer/nanakshahi-js/blob/fc9b49ca67d1947fa3fba74c7cddf39535698326/lib/getNanakshahiDate.js
 
-	day_diff = Math.floor((inp_date_eng - ref_date_eng) / (1000 * 3600 * 24));
-	month_diff = 0;
-	year_diff = 0;
-	stop_loop = false;
-	start_year = '74';
-	while (!stop_loop) {
-		if (day_diff > nepcal[start_year]['tot_days']) {
-			year_diff++;
-			day_diff -= nepcal[start_year]['tot_days'];
-		}
-		else {
-			for (i = 0; i < nepcal[start_year]['mon_days'].length; i++) {
-				days_mon = nepcal[start_year]['mon_days'][i];
-				if (day_diff >= days_mon) {
-					month_diff++;
-					day_diff -= days_mon;
-				}
-				else {
-					stop_loop = true;
-					break;
-				}
-			}
-		}
-		if (!stop_loop) {
-			start_year++;
-		}
-	}
-	date_diff = [year_diff, month_diff, day_diff];
-	for (i = 0; i < ref_date_nep.length; i++) {
-		date_diff[i] += ref_date_nep[i];
-		if (i == 1) {
-			if (date_diff[i] >= 13) {
-				date_diff[i] -= 12;
-				date_diff[i - 1] += 1;
-			}
-			month = months[date_diff[i] - 1];
-		}
-	}
-	nepDate = "|          " + month + " " + date_diff[2] + ", " + date_diff[0];
-	text = new St.Label({
-		style_class: 'nepcal-label',
-		y_expand: true,
-		y_align: Clutter.ActorAlign.CENTER,
-		text: nepDate + " B.S."
-	});
-	button.set_child(text);
-	sourceId = Mainloop.timeout_add_seconds(1, _getNepaliDate);
+  months = ['Chet', 'Vaisakh', 'Jeth', 'Harh', 'Savan', 'Bhadon', 'Assu', 'Katak', 'Maghar', 'Poh', 'Magh', 'Phagun'];
+
+  // NS Month Offsets
+  monthOffsets = [ 14, 14, 15, 15, 16, 16, 15, 15, 14, 14, 13, 12 ];
+
+  // Current Date
+  gregorianDate = new Date();
+  year = gregorianDate.getFullYear();
+
+  // Calculate Nanakshahi Year - March 14 (1 Chet) Nanakshahi New Year
+  nsYear = gregorianDate >= new Date( year, 2, 14 )
+    ? year - 1468
+    : year - 1469;
+
+  // Calculate Nanakshahi Date
+  nsMonth = ( gregorianDate.getMonth() + 9 ) % 12;
+  nsNextMonth = ( nsMonth + 1 ) % 12;
+
+  if ( gregorianDate.getDate() >= monthOffsets[ nsNextMonth ] ) {
+    nsMonth = nsNextMonth;
+    nsDate = gregorianDate.getDate() - monthOffsets[ nsNextMonth ] + 1;
+  } else {
+    gregorianMonths = [
+      31,
+      leapYear( year) ? 29 : 28,
+      31,
+      30,
+      31,
+      30,
+      31,
+      31,
+      30,
+      31,
+      30,
+      31,
+    ];
+    nsDate = gregorianMonths[ ( gregorianDate.getMonth() + 11 ) % 12 ]
+      - monthOffsets[ nsMonth ] + gregorianDate.getDate() + 1;
+  }
+
+  date = "|          " + nsDate + " " + months[ nsMonth ] + ", " + nsYear + " N.S.";
+  text = new St.Label({
+    style_class: 'nanakshahi-label',
+    y_expand: true,
+    y_align: Clutter.ActorAlign.CENTER,
+    text: date
+  });
+  button.set_child(text);
+  sourceId = Mainloop.timeout_add_seconds(1, _getNanakshahDate);
 }
 
 function init() { }
 
 function enable() {
-	button = new St.Bin({
-		style_class: 'panel-button',
-		reactive: true,
-		can_focus: true,
-		x_expand: true,
-		y_expand: false,
-		track_hover: false
-	});
+  button = new St.Bin({
+    style_class: 'panel-button',
+    reactive: true,
+    can_focus: true,
+    x_expand: true,
+    y_expand: false,
+    track_hover: false
+  });
 
-	_getNepaliDate();
+  _getNanakshahiDate();
 
-	Main.panel._centerBox.insert_child_at_index(button, -1);
+  Main.panel._centerBox.insert_child_at_index(button, -1);
 }
 
 function disable() {
-	Main.panel._centerBox.remove_child(button);
-	if (button) {
-		button.destroy();
-		buttton = null;
-	}
-	if (sourceId) {
-		Mainloop.source_remove(sourceId);
-		sourceId = null;
-	}
+  Main.panel._centerBox.remove_child(button);
+  if (button) {
+    button.destroy();
+    buttton = null;
+  }
+  if (sourceId) {
+    Mainloop.source_remove(sourceId);
+    sourceId = null;
+  }
 }
